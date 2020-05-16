e156001ece45b3ba1e12c5c71830e936f9e68dfb
samuel <samuel>
2020-05-17 01:39:38 +0200

    Revert "change"
    This reverts commit 4b9ae53ff5b35915f49ce0e4bfea4ec1b2a8bacf.

--- betterdiscord/plugin/src/ui/icons.js
+++
@@ -1,4 +0,0 @@
-export {default as IconError} from "./icons/error";
-export {default as IconInfo} from "./icons/info";
-export {default as IconSuccess} from "./icons/success";
-export {default as IconWarning} from "./icons/warning";
--- src/webpackmodules.js
+++ src/webpackmodules.js
@@ -1,324 +1,104 @@
+const req = webpackJsonp.push([
+	[],
+	{ __extra_id__: (module, exports, req) => (module.exports = req) },
+	[["__extra_id__"]],
+]);
+delete req.m.__extra_id__;
+delete req.c.__extra_id__;
+const find = (filter) => {
+	for (const i in req.c) {
+		if (req.c.hasOwnProperty(i)) {
+			const m = req.c[i].exports;
+			if (m && m.__esModule && m.default && filter(m.default)) return m.default;
+			if (m && filter(m)) return m;
+		}
+	}
+	// console.warn("Cannot find loaded module in cache");
+	return null;
+};
 
- /**
- * Checks if a given module matches a set of parameters.
- * @callback module:WebpackModules.Filters~filter
- * @param {*} module - module to check
- * @returns {boolean} - True if the module matches the filter, false otherwise
- */
+const findAll = (filter) => {
+	const modules = [];
+	for (const i in req.c) {
+		if (req.c.hasOwnProperty(i)) {
+			const m = req.c[i].exports;
+			if (m && m.__esModule && m.default && filter(m.default)) modules.push(m.default);
+			else if (m && filter(m)) modules.push(m);
+		}
+	}
+	return modules;
+};
 
-/**
- * Filters for use with {@link module:WebpackModules} but may prove useful elsewhere.
- */
-class Filters {
-    /**
-     * Generates a {@link module:WebpackModules.Filters~filter} that filters by a set of properties.
-     * @param {Array<string>} props - Array of property names
-     * @param {module:WebpackModules.Filters~filter} filter - Additional filter
-     * @returns {module:WebpackModules.Filters~filter} - A filter that checks for a set of properties
-     */
-    static byProperties(props, filter = m => m) {
-        return module => {
-            const component = filter(module);
-            if (!component) return false;
-            return props.every(property => component[property] !== undefined);
-        };
-    }
+const findByProps = (...propNames) => find((module) => propNames.every((prop) => module[prop] !== undefined));
+const findByPrototypes = (...protoNames) =>
+	find((module) => module.prototype && protoNames.every((protoProp) => module.prototype[protoProp] !== undefined));
+const findByDisplayName = (displayName) => find((module) => module.displayName === displayName);
 
-    /**
-     * Generates a {@link module:WebpackModules.Filters~filter} that filters by a set of properties on the object's prototype.
-     * @param {Array<string>} fields - Array of property names
-     * @param {module:WebpackModules.Filters~filter} filter - Additional filter
-     * @returns {module:WebpackModules.Filters~filter} - A filter that checks for a set of properties on the object's prototype
-     */
-    static byPrototypeFields(fields, filter = m => m) {
-        return module => {
-            const component = filter(module);
-            if (!component) return false;
-            if (!component.prototype) return false;
-            return fields.every(field => component.prototype[field] !== undefined);
-        };
-    }
+// window.WebpackModules = { find, findAll, findByProps, findByPrototypes, findByDisplayName };
 
-    /**
-     * Generates a {@link module:WebpackModules.Filters~filter} that filters by a regex.
-     * @param {RegExp} search - A RegExp to check on the module
-     * @param {module:WebpackModules.Filters~filter} filter - Additional filter
-     * @returns {module:WebpackModules.Filters~filter} - A filter that checks for a set of properties
-     */
-    static byCode(search, filter = m => m) {
-        return module => {
-            const method = filter(module);
-            if (!method) return false;
-            return method.toString([]).search(search) !== -1;
-        };
-    }
+window.req = webpackJsonp.push([[], {
+    '__extra_id__': (module, exports, req) => module.exports = req
+}, [['__extra_id__']]]);
+delete req.m['__extra_id__'];
+delete req.c['__extra_id__'];
 
-    /**
-     * Generates a {@link module:WebpackModules.Filters~filter} that filters by strings.
-     * @param {...String} search - A RegExp to check on the module
-     * @returns {module:WebpackModules.Filters~filter} - A filter that checks for a set of strings
-     */
-    static byString(...strings) {
-        return module => {
-            const moduleString = module.toString([]);
-            for (const s of strings) {
-                if (!moduleString.includes(s)) return false;
-            }
-            return true;
-        };
-    }
-
-    /**
-     * Generates a {@link module:WebpackModules.Filters~filter} that filters by a set of properties.
-     * @param {string} name - Name the module should have
-     * @param {module:WebpackModules.Filters~filter} filter - Additional filter
-     * @returns {module:WebpackModules.Filters~filter} - A filter that checks for a set of properties
-     */
-    static byDisplayName(name) {
-        return module => {
-            return module && module.displayName === name;
-        };
-    }
-
-    /**
-     * Generates a combined {@link module:WebpackModules.Filters~filter} from a list of filters.
-     * @param {...module:WebpackModules.Filters~filter} filters - A list of filters
-     * @returns {module:WebpackModules.Filters~filter} - Combinatory filter of all arguments
-     */
-    static combine(...filters) {
-        return module => {
-            return filters.every(filter => filter(module));
-        };
-    }
-}
-
-window.WebpackModules =  class WebpackModules {
-
-    static find(filter, first = true) {return this.getModule(filter, first);}
-    static findAll(filter) {return this.getModule(filter, false);}
-    static findByUniqueProperties(props, first = true) {return first ? this.getByProps(...props) : this.getAllByProps(...props);}
-    static findByDisplayName(name) {return this.getByDisplayName(name);}
-
-    /**
-     * Finds a module using a filter function.
-     * @param {Function} filter A function to use to filter modules
-     * @param {Boolean} first Whether to return only the first matching module
-     * @return {Any}
-     */
-    static getModule(filter, first = true) {
-        const modules = this.getAllModules();
-        const rm = [];
-        for (const index in modules) {
-            if (!modules.hasOwnProperty(index)) continue;
-            const module = modules[index];
-            const {exports} = module;
-            let foundModule = null;
-
-            if (!exports) continue;
-            if (exports.__esModule && exports.default && filter(exports.default)) foundModule = exports.default;
-            if (filter(exports)) foundModule = exports;
-            if (!foundModule) continue;
-            if (first) return foundModule;
-            rm.push(foundModule);
+window.findModule = (module, silent) => {
+    for (let i in req.c) {
+        if (req.c.hasOwnProperty(i)) {
+            let m = req.c[i].exports;
+            if (m && m.__esModule && m.default && m.default[module] !== undefined)
+                return m.default;
+            if (m && m[module] !== undefined)
+                return m;
         }
-        return first || rm.length == 0 ? undefined : rm;
     }
-
-    /**
-     * Gets a specific module by index of the webpack require cache.
-     * Best used in combination with getIndex in order to patch a
-     * specific function.
-     *
-     * Note: this gives the **raw** module, meaning the actual module
-     * is in returnValue.exports. This is done in order to be able
-     * to patch modules which export a single function directly.
-     * @param {Number} index Index into the webpack require cache
-     * @return {Any}
-     */
-
-    /**
-     * Gets the index in the webpack require cache of a specific
-     * module using a filter.
-     * @param {Function} filter A function to use to filter modules
-     * @return {Number|null}
-     */
-    static getIndex(filter) {
-        const modules = this.getAllModules();
-        for (const index in modules) {
-            if (!modules.hasOwnProperty(index)) continue;
-            const module = modules[index];
-            const {exports} = module;
-            let foundModule = null;
-
-            if (!exports) continue;
-            if (exports.__esModule && exports.default && filter(exports.default)) foundModule = exports.default;
-            if (filter(exports)) foundModule = exports;
-            if (!foundModule) continue;
-            return index;
+    return null;
+};
+window.findModules = (module) => {
+    let mods = [];
+    for (let i in req.c) {
+        if (req.c.hasOwnProperty(i)) {
+            let m = req.c[i].exports;
+            if (m && m.__esModule && m.default && m.default[module] !== undefined)
+                mods.push(m.default);
+            if (m && m[module] !== undefined)
+                mods.push(m);
         }
-        return null;
     }
-
-    /**
-     * Gets the index in the webpack require cache of a specific
-     * module that was already found.
-     * @param {Any} module An already acquired module
-     * @return {Number|null}
-     */
-    static getIndexByModule(module) {
-        return this.getIndex(m => m == module);
+    return mods;
+};
+window.findRawModule = (module, silent) => {
+    for (let i in req.c) {
+        if (req.c.hasOwnProperty(i)) {
+            let m = req.c[i].exports;
+            if (m && m.__esModule && m.default && m.default[module] !== undefined)
+                return req.c[i];
+            if (m && m[module] !== undefined)
+                return req.c[i];
+        }
     }
-
-    /**
-     * Finds all modules matching a filter function.
-     * @param {Function} filter A function to use to filter modules
-     */
-    static getModules(filter) {return this.getModule(filter, false);}
-
-    /**
-     * Finds a module by its name.
-     * @param {String} name The name of the module
-     * @param {Function} fallback A function to use to filter modules if not finding a known module
-     * @return {Any}
-     */
-    static getModuleByName(name, fallback) {
-        if (DiscordModules.hasOwnProperty(name)) return DiscordModules[name];
-        if (!fallback) return undefined;
-        const module = this.getModule(fallback, true);
-        return module ? DiscordModules[name] = module : undefined;
-    }
-
-    /**
-     * Finds a module by its display name.
-     * @param {String} name The display name of the module
-     * @return {Any}
-     */
-    static getByDisplayName(name) {
-        return this.getModule(Filters.byDisplayName(name), true);
-    }
-
-    /**
-     * Finds a module using its code.
-     * @param {RegEx} regex A regular expression to use to filter modules
-     * @param {Boolean} first Whether to return the only the first matching module
-     * @return {Any}
-     */
-    static getByRegex(regex, first = true) {
-        return this.getModule(Filters.byCode(regex), first);
-    }
-
-    /**
-     * Finds a single module using properties on its prototype.
-     * @param {...string} prototypes Properties to use to filter modules
-     * @return {Any}
-     */
-    static getByPrototypes(...prototypes) {
-        return this.getModule(Filters.byPrototypeFields(prototypes), true);
-    }
-
-    /**
-     * Finds all modules with a set of properties of its prototype.
-     * @param {...string} prototypes Properties to use to filter modules
-     * @return {Any}
-     */
-    static getAllByPrototypes(...prototypes) {
-        return this.getModule(Filters.byPrototypeFields(prototypes), false);
-    }
-
-    /**
-     * Finds a single module using its own properties.
-     * @param {...string} props Properties to use to filter modules
-     * @return {Any}
-     */
-    static getByProps(...props) {
-        return this.getModule(Filters.byProperties(props), true);
-    }
-
-    /**
-     * Finds all modules with a set of properties.
-     * @param {...string} props Properties to use to filter modules
-     * @return {Any}
-     */
-    static getAllByProps(...props) {
-        return this.getModule(Filters.byProperties(props), false);
-    }
-
-    /**
-     * Finds a single module using a set of strings.
-     * @param {...String} props Strings to use to filter modules
-     * @return {Any}
-     */
-    static getByString(...strings) {
-        return this.getModule(Filters.byString(...strings), true);
-    }
-
-    /**
-     * Finds all modules with a set of strings.
-     * @param {...String} strings Strings to use to filter modules
-     * @return {Any}
-     */
-    static getAllByString(...strings) {
-        return this.getModule(Filters.byString(...strings), false);
-    }
-
-    /**
-     * Gets a specific module by index of the webpack require cache.
-     * Best used in combination with getIndex in order to patch a
-     * specific function.
-     * @param {Number} index Index into the webpack require cache
-     * @return {Any}
-     */
-    static getByIndex(index) {
-        return WebpackModules.require.c[index].exports;
-    }
-
-    /**
-     * Discord's __webpack_require__ function.
-     */
-    static get require() {
-        if (this._require) return this._require;
-        const id = "zl-webpackmodules";
-        const __webpack_require__ = typeof(window.webpackJsonp) == "function" ? window.webpackJsonp([], {
-            [id]: (module, exports, __webpack_require__) => exports.default = __webpack_require__
-        }, [id]).default : window.webpackJsonp.push([[], {
-            [id]: (module, exports, __webpack_require__) => module.exports = __webpack_require__
-        }, [[id]]]);
-        delete __webpack_require__.m[id];
-        delete __webpack_require__.c[id];
-        return this._require = __webpack_require__;
-    }
-
-    /**
-     * Returns all loaded modules.
-     * @return {Array}
-     */
-    static getAllModules() {
-        return this.require.c;
-    }
-
-}
-
-
-window.monkeyPatch = function (what, methodName, newFunc) {
-	if (!what || typeof what !== "object") return false;
-	const displayName = what.displayName || what.name || what.constructor.displayName || what.constructor.name;
-	const origMethod = what[methodName];
-	const cancel = () => {
-		what[methodName] = origMethod;
-		return true;
-	};
-	what[methodName] = function () {
-		const data = {
-			thisObject: this,
-			methodArguments: arguments,
-			originalMethod: origMethod,
-			callOriginalMethod: () =>
-				(data.returnValue = data.originalMethod.apply(data.thisObject, data.methodArguments)),
-		};
-		return newFunc(data);
-	};
-	what[methodName].__monkeyPatched = true;
-	what[methodName].displayName = "patched " + (what[methodName].displayName || methodName);
-	what[methodName].unpatch = cancel;
-	return true;
+    return null;
+};
+window.monkeyPatch = function(what, methodName, newFunc) {
+    if (!what || typeof what !== 'object')
+        return false;
+    const displayName = what.displayName || what.name || what.constructor.displayName || what.constructor.name;
+    const origMethod = what[methodName];
+    const cancel = () => {
+        what[methodName] = origMethod;
+        return true;
+    };
+    what[methodName] = function() {
+        const data = {
+            thisObject: this,
+            methodArguments: arguments,
+            originalMethod: origMethod,
+            callOriginalMethod: () => data.returnValue = data.originalMethod.apply(data.thisObject, data.methodArguments)
+        };
+        return newFunc(data);
+    };
+    what[methodName].__monkeyPatched = true;
+    what[methodName].displayName = 'patched ' + (what[methodName].displayName || methodName);
+    what[methodName].unpatch = cancel;
+    return true;
 };
