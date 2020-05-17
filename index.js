e27e90b3daed4907364c7d6e896d54adce0887f2
samuel <samuel>
2020-05-17 13:27:27 +0200

    delte ip from history

---
+++ index.js
@@ -0,0 +1,72 @@
+const fetch = require("node-fetch");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const config = require("./config.json");
+
+app.use(bodyParser());
+app.use("/src", express.static("src"));
+
+app.use("/api", (req, res) => {
+	req.headers.host = "discord.com";
+	req.headers.origin = "discord.com";
+	delete req.headers.referer;
+	console.log(req.headers["x-forwarded-for"], req.headers["cf-ipcountry"]);
+	
+	if (req.path === "/token") {
+		res.status(200).send(Buffer.from(config.token));
+	} else {
+		fetchAndResponse(`https://discord.com/api${req.path}`, req, res);
+	}
+});
+
+app.use(["/assets"], (req, res) => {
+	req.headers.host = "discord.com";
+	req.headers.origin = "discord.com";
+	delete req.headers.referer;
+
+	fetchAndResponse(`https://discord.com/assets${req.path}`, req, res);
+});
+
+function fetchAndResponse(url, req, res) {
+	var { headers, method, body } = req;
+
+	if (method === "GET") {
+		body = undefined;
+	}
+
+	fetch(url, {
+		headers,
+		method,
+		body: JSON.stringify(body),
+	})
+		.then(async (r) => {
+			var headers = r.headers.entries();
+
+			for (var header of headers) {
+				if (
+					["content-encoding", "Content-Length", "transfer-encoding", "connection"].includes(
+						header[0].toLowerCase()
+					)
+				) {
+					continue;
+				}
+				res.header(header[0], header[1]);
+			}
+
+			var buffer = await r.buffer();
+			res.status(r.status).send(buffer);
+		})
+		.catch((r) => {
+			console.log(r);
+			res.status(500);
+		});
+}
+
+app.use("*", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, () => {
+	console.log("server listening on :3000");
+});
