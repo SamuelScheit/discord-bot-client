ec3d41786dd7a52e3076fbcee970a86b27110f83
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2020-06-26 12:31:38 +0200

    fix cloudflare cookie

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -3,28 +3,33 @@ async function handleRequests(request) {
 	request = new Request(request);
 	var url = new URL(request.url);
 	var response;
+	var origin = Object.fromEntries(request.headers).origin || "https://discordclient.com";
+	console.log(origin, Object.fromEntries(request.headers))
 	try {
 		var u = "https://discord.com" + url.pathname;
 		request.headers.origin = "https://discord.com"
 
 		var options = request;
 		if (u.includes("assets")) {
-			options.cf = {
-				cacheTtl: 31536000,
-				cacheKey: u,
-				cacheEverything: true,
+			options = {
+				cf: {
+					cacheTtl: 31536000,
+					cacheKey: u,
+					cacheEverything: true,
+				}
 			};
 		}
 		response = await fetch(u, options);
 		console.log(u, request)
-		response = new Response(response.body, response);
+		response = new Response(response.body, { ...response, status: 200, statusText: "OK" });
 	} catch (e) {
 		response = new Response(e.toString());
 	}
 
-	response.headers.set("Access-Control-Allow-Method", "POST, GET, OPTIONS, DELETE, PUT");
-	response.headers.set("Access-Control-Allow-Origin", "*");
-	response.headers.set("Access-Control-Allow-Headers", "authorization,content-type,x-failed-requests,x-fingerprint,x-super-properties");
+	response.headers.set("access-control-allow-method", "POST, GET, OPTIONS, DELETE, PUT");
+	response.headers.set("access-control-allow-origin", origin);
+	response.headers.set("access-control-allow-headers", "cookie,authorization,content-type,x-failed-requests,x-fingerprint,x-super-properties");
+	response.headers.set("access-control-allow-credentials", "true")
 	console.log("response");
 	return response;
 }
