01283639b6af38e9558e49fd42424c92276ab2ec
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-25 11:20:58 +0200

    :bug: fix asset loading

--- electron/index.js
+++ electron/index.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, systemPreferences } = require("electron");
 const fetch = require("node-fetch");
 const btoa = require("btoa");
-// const fs = require("fs");
+const fs = require("fs");
 
 async function createWindow() {
 	var html = await fetch("https://raw.githubusercontent.com/Flam3rboy/discord-bot-client/master/index.html");
@@ -28,7 +28,7 @@ async function createWindow() {
 		e.preventDefault();
 		require("electron").shell.openExternal(url);
 	});
-	win.loadURL("https://blank.org");
+	win.loadURL("https://discord.com");
 	// win.loadURL("data:text/html;charset=UTF-8," + encodeURIComponent(html), {
 	// 	baseURLForDataURL: `file://${__dirname}/app`,
 	// });
@@ -39,25 +39,25 @@ async function createWindow() {
 	const { session } = win.webContents;
 
 	session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-		details.requestHeaders["origin"] = "https://canary.discord.com";
 		if (
 			[
-				"https://canary.discord.com/api/v9/users/@me/library",
-				"https://canary.discord.com/api/v9/users/@me/guilds/premium/subscriptions",
-				"https://canary.discord.com/api/v9/science",
+				"https://discord.com/api/v9/users/@me/library",
+				"https://discord.com/api/v9/users/@me/guilds/premium/subscriptions",
+				"https://discord.com/api/v9/science",
 			].includes(details.url) ||
-			details.url.includes("https://canary.discord.com/api/v9/users/@me/billing/trials/") ||
-			details.url.includes("https://canary.discord.com/api/v9/users/@me/applications/")
+			details.url.includes("https://discord.com/api/v9/users/@me/billing/trials/") ||
+			details.url.includes("https://discord.com/api/v9/users/@me/applications/")
 		) {
 			return callback({ cancel: true });
 		}
-		delete details.requestHeaders["User-Agent"];
+		details.requestHeaders["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
 
 		callback({ requestHeaders: details.requestHeaders });
 	});
 
 	session.webRequest.onHeadersReceived(filter, (details, callback) => {
 		details.responseHeaders["access-control-allow-origin"] = "*";
+		details.responseHeaders["content-security-policy"] ="default-src * data: 'unsafe-eval' 'unsafe-inline' blob:"
 
 		callback({ responseHeaders: details.responseHeaders });
 	});
